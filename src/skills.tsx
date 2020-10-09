ddfc4da2367b91d7ec4ba2af67d1e88aa3382ea5
doriasu <takuandtetsu@live.jp>
2020-10-10 01:21:49 +0900

    skill説明(とりあえず文字を)追加

--- src/skills.tsx
+++ src/skills.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import {makeStyles,createStyles,Theme} from "@material-ui/core/styles";
 import Icon from './shakura.jpg'
-import {Grid, Typography} from "@material-ui/core";
+import {Button, CardActions, Grid, Typography} from "@material-ui/core";
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 //styleを定義
 const useStyles=makeStyles((theme:Theme)=>
     createStyles({
@@ -19,8 +21,14 @@ const useStyles=makeStyles((theme:Theme)=>
         img:{
             width: '100%',
             height:'100%',
-            objectFit:'scale-down'
+            objectFit:'scale-down',
+            marginBottom:'1px'
         },
+        txt:{
+            display:'flex',
+            alignItems:'center',
+            justifyContent: 'center'
+        }
 
     })
 );
@@ -36,17 +44,38 @@ function Skill(){
                 Skills
             </h2>
             <Grid container className={classes.ul}>
-                <Grid item xs={3}><img src={`${process.env.PUBLIC_URL}/skill_pic/144px-The_C_Programming_Language_logo.svg.png`} className={classes.img} /></Grid>
-                <Grid item xs={3}><img src={`${process.env.PUBLIC_URL}/skill_pic/cpp_logo_out.png`} className={classes.img} /></Grid>
-                <Grid item xs={3}><img src={`${process.env.PUBLIC_URL}/skill_pic/Logo_Java_out.jpg`} className={classes.img} /></Grid>
-                <Grid item xs={3}><img src={`${process.env.PUBLIC_URL}/skill_pic/python-logo.png`}  className={classes.img}/></Grid>
+                <Grid item xs={3}><img src={`${process.env.PUBLIC_URL}/skill_pic/144px-The_C_Programming_Language_logo.svg.png`} className={classes.img} />
+                    <Card className={classes.root}>
+                        <CardContent>
+                            <Typography className={classes.txt} color="textSecondary">
+                                Word of the Day
+                            </Typography>
+                        </CardContent>
+
+                    </Card>
+                <div className={classes.txt}>
+                    <ul><li>3years experiences</li><li>making C compiler</li><li>made QNX driver</li></ul>
+                </div></Grid>
+                <Grid item xs={3}><img src={`${process.env.PUBLIC_URL}/skill_pic/cpp_logo_out.png`} className={classes.img} /><div className={classes.txt}>
+                    <ul><li>3years experiences</li><li>competitive programming</li></ul>
+                </div></Grid>
+                <Grid item xs={3}><img src={`${process.env.PUBLIC_URL}/skill_pic/Logo_Java_out.jpg`} className={classes.img} /><div className={classes.txt}>
+                    <ul><li>6months experiences</li><li>for beginning of my programming</li></ul>
+                </div></Grid>
+                <Grid item xs={3}><img src={`${process.env.PUBLIC_URL}/skill_pic/python-logo.png`}  className={classes.img}/><div className={classes.txt}>
+                    <ul><li>2years experiences</li><li>making tools(for slack or discord)</li><li>WebScraping</li><li>machine learning</li></ul>
+                </div></Grid>
 
             </Grid>
             <Grid container className={classes.ul}>
-                <Grid item xs={3}><img src={`${process.env.PUBLIC_URL}/skill_pic/144px-C_Sharp_wordmark.svg.png`} className={classes.img} /></Grid>
-                <Grid item xs={3}><img src={`${process.env.PUBLIC_URL}/skill_pic/256px-Unity_Technologies_logo.svg.png`} className={classes.img} /></Grid>
-                <Grid item xs={3}><img src={`${process.env.PUBLIC_URL}/skill_pic/ts-100x100.png`} className={classes.img} /></Grid>
-                <Grid item xs={3}><img src={`${process.env.PUBLIC_URL}/skill_pic/react_out.png`}  className={classes.img}/></Grid>
+                <Grid item xs={3}><img src={`${process.env.PUBLIC_URL}/skill_pic/144px-C_Sharp_wordmark.svg.png`} className={classes.img} /><div className={classes.txt}>
+                    <ul><li>4months experiences</li><li>making AR Application(by Unity)</li></ul>
+                </div></Grid>
+
+                <Grid item xs={3}><img src={`${process.env.PUBLIC_URL}/skill_pic/ts-100x100.png`} className={classes.img} /><div className={classes.txt}>
+                    <ul><li>6months experiences</li><li>making this site(by react)</li><li>made many tools(see Github!)</li></ul>
+                </div></Grid>
+
 
             </Grid>
 
